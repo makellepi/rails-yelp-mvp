1521b6a525211d30e8db0f81b04d34fca9d15510
Marta Keller <marta.keller.pie@gmail.com>
2018-02-21 16:15:08 +0000

    excercise restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurants = Restaurant.create([{ name: '100 Montaditos', address: 'Cais do Sodre', phone_number: '915 107 077', category: 'french'}, { name: 'Casa Manolo', address: 'Rossio', phone_number: '915 107 077', category: 'italian'}])
